96a62e74e102068d746602fbe4fc01fd72a427e4
Oleg Sobolev <sobolev_ov@mail.ru>
2021-10-30 19:55:05 +0300

    Simplified conversion to RuntimeException

--- src/main/java/com/lowagie/text/ExceptionConverter.java
+++ src/main/java/com/lowagie/text/ExceptionConverter.java
@@ -1,13 +1,4 @@
 /*
- * The original version of this class was published in an article by professor Heinz Kabutz.
- * Read http://www.javaspecialists.co.za/archive/newsletter.do?issue=033&print=yes&locale=en_US
- * "This material from The Java(tm) Specialists' Newsletter by Maximum Solutions (South Africa).
- * Please contact Maximum Solutions  for more information."
- *
- * Copyright (C) 2001 Dr. Heinz M. Kabutz
- */
-
-/*
  * The contents of this file are subject to the Mozilla Public License Version 1.1
  * (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.mozilla.org/MPL/
@@ -53,9 +44,6 @@
  */
 package com.lowagie.text;
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
-
 /**
  * The ExceptionConverter changes a checked exception into an
  * unchecked exception.
@@ -63,14 +51,6 @@ import java.io.PrintWriter;
 public class ExceptionConverter extends RuntimeException {
 
     private static final long serialVersionUID = 8657630363395849399L;
-    /**
-     * we keep a handle to the wrapped exception
-     */
-    private Exception ex;
-    /**
-     * prefix for the exception
-     */
-    private String prefix;
 
     /**
      * Construct a RuntimeException based on another Exception
@@ -78,101 +58,6 @@ public class ExceptionConverter extends RuntimeException {
      * @param ex the exception that has to be turned into a RuntimeException
      */
     public ExceptionConverter(Exception ex) {
-        this.ex = ex;
-        prefix = (ex instanceof RuntimeException) ? "" : "ExceptionConverter: ";
-    }
-
-    /**
-     * Convert an Exception into an unchecked exception. Return the exception if it is
-     * already an unchecked exception or return an ExceptionConverter wrapper otherwise
-     *
-     * @param ex the exception to convert
-     * @return an unchecked exception
-     * @since 2.1.6
-     */
-    public static RuntimeException convertException(Exception ex) {
-        if (ex instanceof RuntimeException) {
-            return (RuntimeException) ex;
-        }
-        return new ExceptionConverter(ex);
-    }
-
-    /**
-     * and allow the user of ExceptionConverter to get a handle to it.
-     *
-     * @return the original exception
-     */
-    public Exception getException() {
-        return ex;
-    }
-
-    /**
-     * We print the message of the checked exception
-     *
-     * @return message of the original exception
-     */
-    public String getMessage() {
-        return ex.getMessage();
-    }
-
-    /**
-     * and make sure we also produce a localized version
-     *
-     * @return localized version of the message
-     */
-    public String getLocalizedMessage() {
-        return ex.getLocalizedMessage();
-    }
-
-    /**
-     * The toString() is changed to be prefixed with ExceptionConverter
-     *
-     * @return String version of the exception
-     */
-    public String toString() {
-        return prefix + ex;
-    }
-
-    /**
-     * we have to override this as well
-     */
-    public void printStackTrace() {
-        printStackTrace(System.err);
-    }
-
-    /**
-     * here we prefix, with s.print(), not s.println(), the stack
-     * trace with "ExceptionConverter:"
-     *
-     * @param s
-     */
-    public void printStackTrace(PrintStream s) {
-        synchronized (s) {
-            s.print(prefix);
-            ex.printStackTrace(s);
-        }
-    }
-
-    /**
-     * Again, we prefix the stack trace with "ExceptionConverter:"
-     *
-     * @param s
-     */
-    public void printStackTrace(PrintWriter s) {
-        synchronized (s) {
-            s.print(prefix);
-            ex.printStackTrace(s);
-        }
-    }
-
-    /**
-     * requests to fill in the stack trace we will have to ignore.
-     * We can't throw an exception here, because this method
-     * is called by the constructor of Throwable
-     *
-     * @return a Throwable
-     */
-    public Throwable fillInStackTrace() {
-        return this;
+        super(ex);
     }
 }
