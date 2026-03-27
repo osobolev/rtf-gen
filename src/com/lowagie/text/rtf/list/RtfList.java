0698593003d5cf50b1114163e494c3c2ca89f68e
Oleg Sobolev <sobolev_ov@mail.ru>
2026-03-27 18:00:23 +0100

    Removed unused private constants

--- src/com/lowagie/text/rtf/list/RtfList.java
+++ src/com/lowagie/text/rtf/list/RtfList.java
@@ -105,22 +105,6 @@ public class RtfList extends RtfElement implements RtfExtendedElement {
      * Constant for the hybrid list
      */
     private static final byte[] LIST_HYBRID = DocWriter.getISOBytes("\\listhybrid");
-    /**
-     * Constant to indicate if the list restarts at each section. Word 7 compatiblity
-     */
-    private static final byte[] LIST_RESTARTHDN = DocWriter.getISOBytes("\\listrestarthdn");
-    /**
-     * Constant for the name of this list
-     */
-    private static final byte[] LIST_NAME = DocWriter.getISOBytes("\\listname");
-    /**
-     * Constant for the identifier of the style of this list. Mutually exclusive with \\liststylename
-     */
-    private static final byte[] LIST_STYLEID = DocWriter.getISOBytes("\\liststyleid");
-    /**
-     * Constant for the identifier of the style of this list. Mutually exclusive with \\liststyleid
-     */
-    private static final byte[] LIST_STYLENAME = DocWriter.getISOBytes("\\liststylename");
 
     // character properties
     /**
