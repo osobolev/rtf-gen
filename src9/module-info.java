14f281ce02a3706bf10397b92a937a4af046bf3e
Oleg Sobolev <sobolev_ov@mail.ru>
2026-03-27 18:02:33 +0100

    Export all packages

--- src9/module-info.java
+++ src9/module-info.java
@@ -3,6 +3,16 @@ module io.github.osobolev.rtf_gen {
     exports com.lowagie.text.factories;
     exports com.lowagie.text.html;
     exports com.lowagie.text.rtf;
+    exports com.lowagie.text.rtf.direct;
+    exports com.lowagie.text.rtf.document;
+    exports com.lowagie.text.rtf.document.output;
+    exports com.lowagie.text.rtf.field;
+    exports com.lowagie.text.rtf.graphic;
+    exports com.lowagie.text.rtf.headerfooter;
+    exports com.lowagie.text.rtf.list;
+    exports com.lowagie.text.rtf.style;
+    exports com.lowagie.text.rtf.table;
+    exports com.lowagie.text.rtf.text;
 
     requires transitive java.desktop;
 }
